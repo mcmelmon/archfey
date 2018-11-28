b84a625dde98a777cc1d70cc6a053f0de5f033ff
Matthew Melmon <matt.melmon@gmail.com>
2018-11-28 13:42:54 -0800

    Map boundaries

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Map : MonoBehaviour {
 
+    public List<Vector3[]> boundaries = new List<Vector3[]>();
+
     Geography geography;
-    int sky_height = 20;
+    Terrain terrain;
+    int sky_height = 100;
 
 
     public struct HeavenAndEarth
@@ -21,7 +24,9 @@ public class Map : MonoBehaviour {
     void Awake ()
     {
         geography = GetComponentInChildren<Geography>();
+        terrain = GetComponentInChildren<Terrain>();
         SetHeavenAndEarth();
+        SetBounds();
     }
 
 
@@ -36,21 +41,55 @@ public class Map : MonoBehaviour {
 
     // private
 
-    private void OnValidate()
-    {
-        if (sky_height < 30) sky_height = 30;
-    }
 
-
-    void AddDirectionBoundary(string direction, Transform boundaries)
+    void AddDirectionBoundaries()
     {
-        // TODO: keep player from flying off map
+        for (int i = 0; i < boundaries.Count; i++)
+        {
+            Vector3[] boundary = boundaries[i];
+            GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            wall.transform.parent = transform;
+            Vector3 heading = boundary[1] - boundary[0];
+            wall.transform.localScale = new Vector3(heading.magnitude, heading.magnitude, 1);
+            wall.transform.gameObject.GetComponentInChildren<Renderer>().enabled = false;
+            wall.transform.up = heading;
+            if (i == 1 || i == 3) wall.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 90));
+            if (i == boundaries.Count - 1) {
+                wall.transform.position = new Vector3(terrain.terrainData.heightmapResolution / 2, sky_height, terrain.terrainData.heightmapResolution / 2);
+                wall.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 90));
+            } else {
+                wall.transform.position = boundary[0] + heading / 2;
+            }
+        }
     }
 
 
     void SetBounds()
     {
+        float resolution = terrain.terrainData.heightmapResolution;
+        Vector3[] north = new Vector3[2], east = new Vector3[2], south = new Vector3[2], west = new Vector3[2], sky = new Vector3[2];
 
+        north[0]    = new Vector3(0, 0, resolution);
+        north[1]    = new Vector3(resolution, 0, resolution);
+        boundaries.Add(north);
+
+        east[0]     = north[1];
+        east[1]     = new Vector3(resolution, 0, 0);
+        boundaries.Add(east);
+
+        south[0]    = east[1];
+        south[1]    = new Vector3(0, 0, 0);
+        boundaries.Add(south);
+
+        west[0]     = south[1];
+        west[1]     = north[0];
+        boundaries.Add(west);
+
+        sky[0] = north[0];
+        sky[1] = south[0];
+        boundaries.Add(sky);
+
+        AddDirectionBoundaries();
     }
 
 
