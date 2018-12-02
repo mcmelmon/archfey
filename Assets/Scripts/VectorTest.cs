41910f69a805d0632184711704458afa3bc8832f
Matthew Melmon <matt.melmon@gmail.com>
2018-12-01 17:26:00 -0800

    Create trail of breadcrumbs

--- Assets/Scripts/VectorTest.cs
+++ Assets/Scripts/VectorTest.cs
@@ -5,23 +5,15 @@ using UnityEngine.AI;
 
 public class VectorTest : MonoBehaviour {
 
-    public NavMeshAgent agent;
-    public Transform first_point;
-    public Transform second_point;
-    public Vector3 to_first;
-    public Vector3 to_second;
-    public float distance = Mathf.Infinity;
-    public Vector3 target_position;
-
-    private GameObject wisp;
-
+    NavMeshAgent agent;
+    public Transform target;
+    GameObject wisp;
+    public List<Vector3> milestones = new List<Vector3>();
+    public List<Vector3> path = new List<Vector3>();
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        to_first = first_point.transform.position - transform.position;
-        to_second = second_point.transform.position - transform.position;
-
         wisp = new GameObject();
         wisp.name = "Wisp";
         wisp.transform.position = transform.position;
@@ -30,22 +22,13 @@ public class VectorTest : MonoBehaviour {
 
 
     void Start () {
+        SetMilestones();
+        PickPath();
     }
 
 
     void Update () {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += transform.TransformDirection(Vector3.forward * 10f * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.position -= transform.TransformDirection(Vector3.forward * 10f * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.LeftArrow)) {
-            CircleLocation(to_first);
-        } else {
-            AgentMotion();
-        }
-
-        to_first = first_point.transform.position - wisp.transform.position;
-        to_second = second_point.transform.position - wisp.transform.position;
+        FollowPath();
     }
 
 
@@ -57,13 +40,9 @@ public class VectorTest : MonoBehaviour {
             Gizmos.DrawRay(wisp.transform.position, wisp.transform.TransformDirection(Vector3.forward * 100));
             Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward * 100));
 
-            Gizmos.color = Color.green;
-            Gizmos.DrawRay(wisp.transform.position, to_first.normalized * 100);
-            Gizmos.DrawRay(transform.position, to_first.normalized * 100);
-
-            Gizmos.color = Color.green;
-            Gizmos.DrawRay(wisp.transform.position, to_second.normalized * 100);
-            Gizmos.DrawRay(transform.position, to_second.normalized * 100);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(wisp.transform.position, (path[0] - wisp.transform.position).normalized * 100);
+            Gizmos.DrawRay(transform.position, (path[0] - wisp.transform.position).normalized * 100);
         }
     }
 
@@ -87,27 +66,55 @@ public class VectorTest : MonoBehaviour {
     }
 
 
-    void AgentMotion()
+    void FollowPath()
     {
         Vector3 forward_motion = wisp.transform.TransformDirection(Vector3.forward);
         forward_motion.y = 0;
 
-        Vector3 central_motion = (to_second - forward_motion);
+        Vector3 central_motion = (path[0] - wisp.transform.position) - forward_motion;
         central_motion.y = 0;
-
+        
         if (Vector3.Angle(forward_motion, central_motion) > 90f)
         {
             Vector3 new_facing = Vector3.RotateTowards(wisp.transform.forward, central_motion, 1f * Time.deltaTime, 0f);
             wisp.transform.rotation = Quaternion.LookRotation(new_facing);
         }
 
-        Vector3 new_position = (forward_motion) * Random.Range(0,3f) * Time.deltaTime;
-
+        Vector3 new_position = (forward_motion) * 5f * Time.deltaTime;
         wisp.transform.position += new_position;
-        if (Vector3.Distance(wisp.transform.position, transform.position) > 10f) {
-            agent.SetDestination(wisp.transform.position);
-        } else {
-            agent.ResetPath();
+
+        agent.SetDestination(wisp.transform.position);
+    }
+
+
+    void PickPath()
+    {
+        foreach (var milestone in milestones)
+        {
+            Circle opportunity = new Circle();
+            opportunity.Inscribe(milestone, 15f);
+            Vector3 breadcrumb = opportunity.RandomContainedPoint();
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = breadcrumb;
+            cube.name = "breadcrumb";
+            path.Add(breadcrumb);
         }
+    }
+
+
+    void SetMilestones()
+    {
+        float position = 4;
+
+        for (int s = 1; s < position - 1; s++)
+        {
+            Vector3 breadcrumb = Vector3.Lerp(transform.position, target.transform.position, s / (position - 1));
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = "Milestone";
+            cube.transform.position = breadcrumb;
+            milestones.Add(breadcrumb);
+        }
+
     }
 }
