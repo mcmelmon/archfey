70a64bc3a947bb16f132e182dbbd3234f128d1b0
Matthew Melmon <matt.melmon@gmail.com>
2018-12-24 09:47:31 -0800

    Add health sliders to units

--- Assets/Scripts/System/Mechanics/Combat/Health.cs
+++ Assets/Scripts/System/Mechanics/Combat/Health.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
+
+    // inspector settings
+
+    public Slider health_bar;
+
 
     // properties
 
     public Actor Actor { get; set; }
     public int CurrentHealth { get; set; }
     public int RecoveryAmount { get; set; }
-    public int StartingHealth { get; set; }
+    public int MaximumHealth { get; set; }
 
 
     // Unity
@@ -18,19 +24,14 @@ public class Health : MonoBehaviour {
     private void Awake()
     {
         Actor = GetComponent<Actor>();
+        StartCoroutine(HealthBarFaceCamera());
     }
 
 
     private void OnValidate()
     {
-        if (StartingHealth < 1) StartingHealth = 1;
+        if (MaximumHealth < 1) MaximumHealth = 1;
         if (CurrentHealth < 0f) CurrentHealth = 0;
-    }
-
-
-    private void Update()
-    {
-        // TODO: prune damagers every now and then to be sure destroyed objects are gone
     }
 
 
@@ -40,21 +41,25 @@ public class Health : MonoBehaviour {
     public void ApplyDamageOverTime()
     {
         // TODO
+        UpdateHealthBar();
     }
 
 
     public void LoseHealth(float amount)
     {
         CurrentHealth -= Mathf.RoundToInt(amount);
+        UpdateHealthBar();
     }
 
 
     public void RecoverHealth(int amount)
     {
-        if (amount == 0 || CurrentHealth == StartingHealth) return;
+        if (amount == 0 || CurrentHealth == MaximumHealth) return;
 
         CurrentHealth += amount;
-        if (CurrentHealth > StartingHealth) CurrentHealth = StartingHealth;
+        if (CurrentHealth > MaximumHealth) CurrentHealth = MaximumHealth;
+
+        UpdateHealthBar();
     }
 
 
@@ -78,7 +83,41 @@ public class Health : MonoBehaviour {
 
     public void SetStartingHealth(int amount)
     {
-        StartingHealth = amount;
+        MaximumHealth = amount;
         CurrentHealth = amount;
+        UpdateHealthBar();
+    }
+
+
+    // private
+
+
+    public float CurrentHealthPercentage()
+    {
+        return (float)CurrentHealth / (float)MaximumHealth;
+    }
+
+
+    private IEnumerator HealthBarFaceCamera()
+    {
+        while (true) {
+            yield return null;
+            Vector3 health_position = transform.position;
+            Vector3 player_position = Player.Instance.viewport.transform.position;
+
+            Quaternion rotation = Quaternion.LookRotation(player_position - health_position, Vector3.up);
+            health_bar.transform.rotation = rotation;
+        }
+    }
+
+
+    public void UpdateHealthBar()
+    {
+        health_bar.value = CurrentHealthPercentage();
+        if (health_bar.value >= 1) {
+            health_bar.gameObject.SetActive(false);
+        } else {
+            health_bar.gameObject.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/World/Map/Flora.cs
+++ Assets/Scripts/World/Map/Flora.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Flora : MonoBehaviour {
 
+    // Inspector settings
+
     public Tree tree_prefab;
     public float tree_coverage;
     public int canopy_layers;
