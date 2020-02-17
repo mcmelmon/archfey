ea450c17701507a67ce12cdc7c8bc44d4dab0143
Matthew Melmon <matt.melmon@gmail.com>
2020-02-16 16:04:43 -0800

    Add ore; miners; multiple warehouses

--- Assets/Scripts/World/Map/Artifice/Industry/Resources.cs
+++ Assets/Scripts/World/Map/Artifice/Industry/Resources.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Resources : MonoBehaviour
 {
-    public enum Category { Timber }
+    public enum Category { Bone = 0, Herbs = 1, Hides = 2, Meat = 3, Ore = 4, Timber = 5, Textile = 6 }
     // properties
 
     public List<Resource> AvailableResources { get; set; }
