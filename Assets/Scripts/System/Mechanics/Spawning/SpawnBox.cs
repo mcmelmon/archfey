b7094b41681569df49d8cb1c58e26ae1c5ea4321
Matthew Melmon <matt.melmon@gmail.com>
2020-02-21 09:54:10 -0800

    Rework spawning

---
+++ Assets/Scripts/System/Mechanics/Spawning/SpawnBox.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBox : MonoBehaviour
+{
+    // properties
+
+    public bool Occupied { get; set; }
+
+    // Unity
+    
+    private void Awake() {
+        Occupied = false;
+        foreach (var collider in Physics.OverlapBox(transform.position, GetComponent<MeshFilter>().mesh.bounds.extents / 2f)) {
+            if (collider.gameObject.GetComponent<SpawnBox>() == null) {
+                Occupied = true;
+                break;
+            }
+        }
+    }
+
+    private void Update() {
+        Occupied = false;
+        foreach (var collider in Physics.OverlapBox(transform.position, GetComponent<MeshFilter>().mesh.bounds.extents / 2f)) {
+            if (collider.gameObject.GetComponent<SpawnBox>() == null) {
+                Occupied = true;
+                break;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+
+    // public
+
+    public GameObject Spawn(GameObject _prefab)
+    {
+        GameObject new_spawn = Instantiate(_prefab, transform.position, _prefab.transform.rotation);
+        Occupied = true;
+        return new_spawn;
+    }
+}
